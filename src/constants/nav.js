50ab732d4e724d93bee8d84e5b1046dbd395fbe7
AdrianGerman <adrian.german1019@gmail.com>
2026-06-26 13:38:10 -0600

    nav js new timeline

---
+++ src/constants/nav.js
@@ -0,0 +1,39 @@
+export const TABS = [
+  {
+    id: "home",
+    label: "Inicio",
+    icon: "ti-home",
+  },
+  {
+    id: "inbox",
+    label: "VODs",
+    icon: "ti-inbox",
+    storeKey: "inbox",
+  },
+  {
+    id: "ideas",
+    label: "Ideas",
+    icon: "ti-bulb",
+    storeKey: "ideas",
+  },
+  {
+    id: "editing",
+    label: "Edición",
+    icon: "ti-cut",
+    storeKey: "editing",
+  },
+  {
+    id: "shorts",
+    label: "Shorts",
+    icon: "ti-device-mobile",
+    storeKey: "shorts",
+  },
+  {
+    id: "trash",
+    label: "Para borrar",
+    icon: "ti-trash",
+    storeKey: "trash",
+  },
+]
+
+export const STORE_KEYS = ["inbox", "ideas", "editing", "shorts", "trash"]
