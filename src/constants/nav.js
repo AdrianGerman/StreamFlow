d1e82b108243808e6a791d90dcee54ad1e0645f9
AdrianGerman <adrian.german1019@gmail.com>
2026-07-16 18:24:46 -0600

    update nav for new fields

--- src/constants/nav.js
+++ src/constants/nav.js
@@ -5,10 +5,18 @@ export const TABS = [
     icon: "ti-home",
   },
   {
-    id: "inbox",
-    label: "VODs",
-    icon: "ti-inbox",
+    id: "streams",
+    label: "Streams",
+    icon: "ti-device-tv",
     storeKey: "inbox",
+    filter: "stream",
+  },
+  {
+    id: "recordings",
+    label: "Grabaciones",
+    icon: "ti-video",
+    storeKey: "inbox",
+    filter: "recording",
   },
   {
     id: "ideas",
@@ -37,3 +45,5 @@ export const TABS = [
 ]
 
 export const STORE_KEYS = ["inbox", "ideas", "editing", "shorts", "trash"]
+
+export const TAB_MAP = Object.fromEntries(TABS.map((t) => [t.id, t]))
